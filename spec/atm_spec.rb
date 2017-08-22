0aebb3a5f3ce7639238d77697878663c6e841e1f
Andrea <andreademoja@gmail.com>
2017-08-22 10:20:27 +0200

    atm machine until account status creation

---
+++ spec/atm_spec.rb
@@ -0,0 +1,53 @@
+require './lib/atm.rb'
+require 'date'
+
+describe Atm do
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '09/17', account_status: :active) }
+
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+
+  it 'has 1000$ on initialized' do
+    expect(subject.funds).to eq 1000
+  end
+
+  it 'funds are reduced at withdraw' do
+    subject.withdraw(50, '1234', account)
+    expect(subject.funds).to eq 950
+  end
+
+  it 'allow withdraw if account has enough balance.' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, '1234', account,)).to eq expected_output
+  end
+
+  it 'rejects withdraw if account has insufficient_funds' do
+    expected_output = { status: false, message: 'insufficient funds', date: Date.today }
+    expect(subject.withdraw(105, '1234', account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if ATM has insufficient funds' do
+    subject.funds = 50
+    expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    expect(subject.withdraw(100, '1234', account)).to eq expected_output
+  end
+
+  it 'reject withdraw if pin is wrong' do
+    expected_output = { status: false, message: 'wrong pin', date: Date.today }
+    expect(subject.withdraw(50, '3512', account)).to eq expected_output
+  end
+
+  it 'reject withdraw if card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(6, '1234', account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if account is disabled' do
+    allow(account).to receive(:account_status).and_return(:disabled)
+    expected_output = { status: false, message: 'account disabled', date: Date.today}
+    expect(subject.withdraw(50,'1234', account)).to eq expected_output
+  end
+end
