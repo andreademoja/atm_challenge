df8243f480cb845a246ace371e08540024bab7cc
Andrea <andreademoja@gmail.com>
2017-08-23 23:25:38 +0200

    refactoring

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,7 +6,6 @@ describe Account do
   subject { described_class.new({owner: person}) }
 
   it 'is expected to have 4 digit pin number on initialize' do
-    pin = 1234
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
   end
